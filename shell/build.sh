03cb041c972566a495128b8ee01e35a2e862e0f4
biyejun <bnbiye@163.com>
2024-03-10 23:01:23 +0800

    feat: 修改sh脚本 测试

--- shell/build.sh
+++ shell/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash  
   
 # 切换到目标目录  
-# cd /www/wwwroot/biye-note/ || { echo "无法进入目录 /www/wwwroot/biye-note"; exit 1; }  
+cd /www/wwwroot/biye-note/ || { echo "无法进入目录 /www/wwwroot/biye-note"; exit 1; }  
   
 # 执行 git pull 命令  
-# git pull || { echo "git pull 执行失败"; exit 1; }  
+git pull || { echo "git pull 执行失败"; exit 1; }  
   
 # 当 git pull 拉取完代码后，执行 pnpm run build  
-# pnpm run build || { echo "pnpm run build 执行失败"; exit 1; }  
+pnpm run build || { echo "pnpm run build 执行失败"; exit 1; }  
   
 # 打印执行完毕的信息  
 echo "代码拉取和构建成功完成。"
